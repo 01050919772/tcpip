ff486066c285d4407d0dd3ef0236f08da82307d2
student <student@M11039>
2018-01-30 13:58:55 +0900

    bb

--- src/tcpip1/Client.java
+++ src/tcpip1/Client.java
@@ -1,7 +1,42 @@
 package tcpip1;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class Client {
-    public static void main(String [] args){
-      System.out.println("Git);
-    }
+	public static void main(String[] args) {
+		String ip = "70.12.111.130";
+		int port = 7777;
+		Socket socket = null;
+		OutputStream outs = null;
+		OutputStreamWriter outsw = null;
+		try {
+			System.err.println("Start Client");
+			socket = new Socket(ip, port);
+			System.out.println("Connected OK");
+			// Send Data
+			outs = socket.getOutputStream();
+			outsw = new OutputStreamWriter(outs);
+			outsw.write("jh");
+			System.out.println("Send Completed");
+			
+		} catch (UnknownHostException e) {
+			System.out.println("UnknownHostException");
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IOException");
+			e.printStackTrace();
+		} finally {
+			try {
+				outsw.close();
+				outs.close();
+				socket.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 }
